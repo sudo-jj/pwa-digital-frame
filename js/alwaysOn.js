b0894df95183b8803e0e8d231b5aaa51b45bf69a
jha-sudo <>
2022-09-22 05:47:17 -0400

    remove status

--- js/alwaysOn.js
+++ js/alwaysOn.js
@@ -1,15 +1,10 @@
-const statusTxt = document.getElementsByClassName('status')[0];
-
 const turnAlwaysOn = async () => {
     if ('wakeLock' in navigator) {
         try {
             const wakeLock = await navigator.wakeLock.request('screen');
-            console.log(wakeLock);
         } catch (ex) {
-            statusTxt.innerText += ex+'\r\n';
         }
     }else{
-        statusTxt.innerText += 'no wakelock\r\n'
     }
 }
 
