48d41d0c21ad302085b649bde8d2808b09a92e96
jha-sudo <>
2022-09-22 04:11:02 -0400

    camera brightness test

---
+++ js/brightnessDetection.js
@@ -0,0 +1,77 @@
+const video = document.createElement('video');
+const canvas = window.canvas = document.createElement('canvas');
+canvas.width = video.width = 200;
+canvas.height = video.height = 150;
+
+const redTHz = 430;
+const greenTHz = 580;
+const blueTHz = 670;
+
+const constraints = {
+    audio: false,
+    video: {
+        facingMode: "user",
+        width: { ideal: video.width },
+        height: { ideal: video.height },
+        frameRate: { ideal: 60 }
+    }
+}
+
+const handleError = (error) => {
+    console.log('navigator.MediaDevices.getUserMedia error: ', error.message, error.name);
+  }
+
+
+const handleSuccess = (stream) => {
+    canvas.getContext('2d').drawImage(video, 0, 0, canvas.width, canvas.height);
+    let = brightnessValue = photonCount();
+
+    console.log( setBrightness(brightnessValue) );
+
+    stopStream(stream);
+}
+
+const getCameraFrame = () => {
+    navigator.mediaDevices.getUserMedia(constraints).then(handleSuccess).catch(handleError);
+}
+
+const stopStream = (stream) => {
+    const tracks = stream.getTracks();
+    tracks.forEach((track) => {
+      track.stop();
+    });
+}
+
+const photonCount = () => {
+    let total = 0;
+    let probability = 3;
+    const img = canvas.getContext('2d').getImageData(0, 0, canvas.width, canvas.height);
+    for (i = 0; i < img.data.length; i += 4) {
+        if (Math.floor(Math.random() * probability) == 0) {
+            total += (img.data[i] * redTHz);
+            total += (img.data[i + 1] * greenTHz);
+            total += (img.data[i + 2] * blueTHz);
+        }
+    }
+    return total * probability;
+}
+
+const setBrightness = (bv) => {
+    return 1 - (bv / maxPhoton);
+}
+
+const _getMaxPhotonCount = () => {
+    let total = 0;
+    for (let x = 0; x < canvas.width; x++) {
+        for (let y = 0; y < canvas.height; y++) {
+            total += (255 * redTHz) + (255 * greenTHz) + (255 * blueTHz);
+        }
+    }
+    return total;
+}
+
+const maxPhoton = _getMaxPhotonCount();
+
+
+setInterval(getCameraFrame, 10 * 1000);
+getCameraFrame();
